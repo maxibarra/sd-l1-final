2ccb070d8cc7082ddb1eccde1c1d0a0afb1b4c99
maxibarra <maxi.8379@gmail.com>
2026-04-29 21:27:30 -0300

    validaciones de id y title

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,48 @@
 import minimist from "minimist";
 import { PelisController } from "./controllers";
 
-
 const pelisController = new PelisController();
 
 async function main() {
- const params = minimist(process.argv.slice(2));
-  
-  if(params._[0] === "add"){
-    const tags = params.tags ? (Array.isArray(params.tags) ? params.tags : [params.tags]) 
-    : [];
+  const params = minimist(process.argv.slice(2));
+
+  if (params._[0] === "add") {
+    const tags = params.tags
+      ? Array.isArray(params.tags)
+        ? params.tags
+        : [params.tags]
+      : [];
     //Lógica para agregar una peli
-    const id = Number(params.id); 
-      if(!id){
-        console.error("El id es obligatorio y debe ser un número válido")
-      }
-    const peli = {id,title: params.title,tags};
+    // validar id antes y cortar la ejecución si no es válido
+    const id = Number(params.id);
+    if (!id) {
+      console.error("El id es obligatorio y debe ser un número válido");
+      return;
+    }
+    // validar título mínimo
+    if (!params.title) {
+      console.error("El title es obligatorio");
+      return;
+    }
+    const peli = { id, title: params.title, tags };
 
     const mensaje = await pelisController.add(peli);
     console.log(mensaje);
-
-  }else if(params._[0] === "get"){
+  } else if (params._[0] === "get") {
     //Obtener una peli por ID
     const peli = await pelisController.getOne({
-      id: Number(params._[1]) 
-    }); 
+      id: Number(params._[1]),
+    });
     console.log(peli);
-    
-  }else if(params._[0] === "search"){
+  } else if (params._[0] === "search") {
     const results = await pelisController.get({
-      search:{title:params.title, tag:params.tag}
+      search: { title: params.title, tag: params.tag },
     });
     console.log(results);
-  }else{
+  } else {
     const pelis = await pelisController.get();
     console.log(pelis);
-  } 
+  }
 }
 
 main();
